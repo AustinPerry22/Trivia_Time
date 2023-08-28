b08a1499a00986848b00cf04af8b10267be87324
Austin <austinjperry22@gmail.com>
2023-08-28 14:08:58 -0600

    template

--- app/models/Question.js
+++ app/models/Question.js
@@ -7,7 +7,17 @@ export class Question {
     }
 
     get questionTemplate() {
-        return `<p>dif: ${this.difficulty} prompt: ${this.prompt}, correct: ${this.correctAnswer}, wrong: ${this.incorrectAnswers}</p>
+        return `<section class="row justify-content-center pb-5">
+        <button class="col-10 card">${this.prompt}</button>
+      </section>
+      <section class="row justify-content-evenly pb-5">
+        <button class="col-4 btn btn-primary">${this.correctAnswer}</button>
+        <button class="col-4 btn btn-primary">${this.incorrectAnswers[0]}</button>
+      </section>
+      <section class="row justify-content-evenly">
+        <button class="col-4 btn btn-primary">${this.incorrectAnswers[1]}</button>
+        <button class="col-4 btn btn-primary">${this.incorrectAnswers[2]}</button>
+      </section>
         `
     }
 }
--- app/router.js
+++ app/router.js
@@ -10,6 +10,13 @@ export const router = [
     path: '',
     controller: QuestionsController,
     view: /*html*/`
+    <section class="row py-5">
+      <div class="col-12 text-center">
+        <img class="img-15 rounded-5"
+          src="https://images.unsplash.com/photo-1599508704512-2f19efd1e35f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cXVlc3Rpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+          alt="question mark image">
+      </div>
+    </section>
     <div id="questions"></div>
     `
   },
